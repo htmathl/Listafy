d072eaa1c89a3714cf86a4c7e624037ba6465537
cesar-lima <cesarr340@gmail.com>
2024-05-28 20:15:51 -0300

    arrumar indentação

--- php/sign-up.php
+++ php/sign-up.php
@@ -2,30 +2,29 @@
 <html lang="pt-br">
     <head>
         <title>Listafy - Sign Up</title>
-        </head>
+    </head>
         
-        <body>
-<?php
-
+    <body>
+        <?php
             $nome = $_POST["txtusername"];
             $email = $_POST["txtemail"];
             $senha = $_POST["txtpassword"];
             $con = mysqli_connect("localhost","root","listafy123");
 
-                if (!$con) {
-                    echo('impossível conectar: '. mysqli_error());
-                } else {
-                    mysqli_select_db($con, "listafy");
-                    $sql = "INSERT INTO usuarios (email, senha, nome)
-                            VALUES ('$email', '$senha', '$nome')";
-                }
-    
-                if (mysqli_query($con, $sql)) {
-                    include '../pages/sign-in.html';
-                } else {
-                    echo('Erro no cadastro: ' . mysqli_error());
-                }
-                mysqli_close($con);
-                ?>
-            </body>
-            </html>
+            if (!$con) {
+                echo('impossível conectar: '. mysqli_error());
+            } else {
+                mysqli_select_db($con, "listafy");
+                $sql = "INSERT INTO usuarios (email, senha, nome)
+                        VALUES ('$email', '$senha', '$nome')";
+            }
+
+            if (mysqli_query($con, $sql)) {
+                include '../pages/sign-in.html';
+            } else {
+                echo('Erro no cadastro: ' . mysqli_error());
+            }
+            mysqli_close($con);                 
+        ?>
+    </body>
+</html>
