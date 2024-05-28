4650b3645f8a82b3c20f72579e502090292c845c
cesar-lima <cesarr340@gmail.com>
2024-05-28 20:15:05 -0300

    implementar codigo para efetuar login

--- php/sign-in.php
+++ php/sign-in.php
@@ -6,7 +6,27 @@
 
     <body>
         <?php
-            
+            $email = $_POST["txtemail"];
+            $senha = $_POST["txtpassword"];
+            $con = mysqli_connect("localhost","root","listafy123");
+
+            if (!$con) {
+                echo('impossível conectar: '. mysqli_error());
+            } else {
+                mysqli_select_db($con, "listafy");
+                $sql = mysqli_query($con, "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'");
+                $id = mysqli_query($con, "SELECT id_usuario FROM usuarios WHERE email = '$email' AND senha = '$senha'");
+
+                if(mysqli_num_rows($sql) == 0) {
+                    echo "odeio php";
+                } else {
+                    $row = mysqli_fetch_assoc($sql);
+                    $id = $row['id_usuario'];
+                    header("Location: ../pages/dashboard.php?id=" . $id);
+                    mysqli_close($con);        
+                }
+            }
+
         ?>
     </body>
 </html>
