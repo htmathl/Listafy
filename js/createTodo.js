f08cec7c1a84dde561af86d2a2f2e75f573b74e1
cesar-lima <cesarr340@gmail.com>
2024-05-29 20:57:07 -0300

    melhorar estilo do modal da funcionalidade de criar todo

--- js/createTodo.js
+++ js/createTodo.js
@@ -1,10 +1,14 @@
 let btnCreate = document.getElementById('btnCreate');
 let popup = document.querySelector('#popup-create-todo');
 let form = document.querySelector('#popup-create-todo > div > form');
+let btnCancelar = document.querySelector('#button-cancel');
 
 btnCreate.addEventListener('click', () => {
     popup.style.display = 'block';
     form.style.display = 'flex';
+});
 
-    
+btnCancelar.addEventListener('click', () => {
+    popup.style.display = 'none';
+    form.style.display = 'none';
 });
